dd1d594c24dae7ee1c974eddfc99c7d10b3ae0da
alex <camward@yandex.ru>
2019-07-25 21:08:18 +0400

    добавление и настройка firebase npm i firebase

--- src/main.js
+++ src/main.js
@@ -8,14 +8,33 @@ import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: 'AIzaSyBfls2MUbea1JSi940bkFyfCRpbapdPTIU',
+  authDomain: 'itc-ads-fc854.firebaseapp.com',
+  databaseURL: 'https://itc-ads-fc854.firebaseio.com',
+  projectId: 'itc-ads-fc854',
+  storageBucket: 'itc-ads-fc854.appspot.com',
+  messagingSenderId: '200678472487'
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
